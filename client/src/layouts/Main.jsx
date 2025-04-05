67f3aeb0828ee713798fa27b05506031e70ca20e
Victor Alexa <victor.d.alexa@gmail.com>
2025-04-05 14:03:03 +0300

    remove logs

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,12 +1,12 @@
-import { Outlet, useLoaderData } from "react-router-dom";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-import { mainFetch } from "../utils/customFetch";
+import { Outlet, useLoaderData } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { mainFetch } from '../utils/customFetch';
 
 export const initialLoader = async () => {
   let authData = {};
   try {
-    const response = await mainFetch("/users/current-user");
+    const response = await mainFetch('/users/current-user');
     authData = { ...authData, response };
     return response.data;
   } catch (error) {
@@ -16,14 +16,13 @@ export const initialLoader = async () => {
 
 const Main = () => {
   const data = useLoaderData();
-  // console.log(data);
+
   return (
     <>
       {<Navbar authData={data} />}
-      {/* {if user then 'loggedNav' else <Navbar/>} */}
+
       <main>
-        <Outlet context={{ data }} />
-        {/* <Outlet /> */}
+        <Outlet context={data} />
       </main>
       <Footer />
     </>
